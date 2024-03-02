9c79f74870221892ae172f48c59feb7c776dc413
ednguyenn <139338857+ednguyenn@users.noreply.github.com>
2024-03-02 13:23:14 +1100

    anything

--- apps/mountduneed.py
+++ apps/mountduneed.py
@@ -1,12 +1,22 @@
 import streamlit as st
 import leafmap.foliumap as leafmap
 
+from samgeo import tms_to_geotiff, split_raster
+from samgeo.text_sam import LangSAM
 
 def app():
 
     st.title("mount-duneed")
 
-    m = leafmap.Map(center=[-38.2426, 144.3056], zoom=19)
+    ##to do: create a GPTAPI to retrieve the latitude and longitude of a input location
+    latitude = -38.2426
+    longitude = 144.3056
+    m = leafmap.Map(center=[latitude, longitude], zoom=19)
     m.add_basemap("SATELLITE")
     
     m.to_streamlit(height=700)
+
+    if m.user_roi_bounds() is not None:
+    bbox = m.user_roi_bounds()
+    else:
+    bbox = [-95.3704, 29.6762, -95.368, 29.6775]
