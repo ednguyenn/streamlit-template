04ae76fbc6acdae96d3cc38442026cb93078b617
ednguyenn <139338857+ednguyenn@users.noreply.github.com>
2024-03-02 14:03:48 +1100

    add crop image

--- apps/mountduneed.py
+++ apps/mountduneed.py
@@ -18,4 +18,11 @@ def app():
 
     if m.user_roi_bounds() is not None:
         bbox = m.user_roi_bounds()
+        image = "satellite.tif"
+        tms_to_geotiff(output=image, bbox=bbox, zoom=20, source="Satellite", overwrite=True)
+        m.layers[-1].visible = False  # turn off the basemap
+        m.add_raster(image, layer_name="Image")
+        
+        m.to_streamlit(height=700)
+        
     else: bbox = [-95.3704, 29.6762, -95.368, 29.6775]
