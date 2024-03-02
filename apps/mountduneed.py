d1f8b76708b45a955a2b5e7a82cc71edcdfd1758
ednguyenn <139338857+ednguyenn@users.noreply.github.com>
2024-03-02 21:19:16 +1100

    d

--- apps/mountduneed.py
+++ apps/mountduneed.py
@@ -1,36 +1,56 @@
 import streamlit as st
 import leafmap.foliumap as leafmap
 import leafmap
-
+from datetime import date
+import geemap
 from samgeo import tms_to_geotiff, split_raster
 from samgeo.text_sam import LangSAM
-
+import ee, folium
+        
 def app():
 
-    st.title("mount-duneed")
+    today = date.today()
 
-    ##to do: create a GPTAPI to retrieve the latitude and longitude of a input location
-    latitude = -38.2426
-    longitude = 144.3056
-    m = leafmap.Map(center=[latitude, longitude], zoom=19)
-    m.add_basemap("SATELLITE")
-    m.to_streamlit(height=700)
+    st.title("Create Satellite Timelapse")
 
-    if m.user_roi_bounds() is not None:
-        bbox = m.user_roi_bounds()
-        image = "satellite.tif"
-        tms_to_geotiff(output=image, bbox=bbox, zoom=20, source="Satellite", overwrite=True)
-        m.layers[-1].visible = False  # turn off the basemap
-        m.add_raster(image, layer_name="Image")
-        sam = LangSAM()
-        text_prompt = "tree"
-        sam.predict(image, text_prompt, box_threshold=0.24, text_threshold=0.24)
-        sam.show_anns(
-            cmap='Greens',
-            add_boxes=False,
-            alpha=0.5,
-            title='Automatic Segmentation of Trees',
-)
-        m.to_streamlit(height=700)   
-        
- 
+    st.markdown(
+        """
+        An interactive web app for creating [Landsat](https://developers.google.com/earth-engine/datasets/catalog/landsat)/[GOES](https://jstnbraaten.medium.com/goes-in-earth-engine-53fbc8783c16) timelapse for any location around the globe. 
+        The app was built using [streamlit](https://streamlit.io), [geemap](https://geemap.org), and [Google Earth Engine](https://earthengine.google.com). For more info, check out my streamlit [blog post](https://blog.streamlit.io/creating-satellite-timelapse-with-streamlit-and-earth-engine). 
+    """
+    )
+
+    row1_col1, row1_col2 = st.columns([2, 1])
+
+    if st.session_state.get("zoom_level") is None:
+        st.session_state["zoom_level"] = 4
+
+    st.session_state["ee_asset_id"] = None
+    st.session_state["bands"] = None
+    st.session_state["palette"] = None
+    st.session_state["vis_params"] = None
+    with row1_col1:
+        ee.Authenticate(token_name="AIzaSyAp0qnp-Ci5SaOD5awEFAZ2_y1WAh74p-4")
+        m = geemap.Map(
+            basemap="HYBRID",
+            plugin_Draw=True,
+            Draw_export=True,
+            locate_control=True,
+            plugin_LatLngPopup=False,
+        )
+        m.add_basemap("ROADMAP")
+
+    with row1_col2:
+
+        keyword = st.text_input("Search for a location:", "")
+        if keyword:
+            locations = geemap.geocode(keyword)
+            if locations is not None and len(locations) > 0:
+                str_locations = [str(g)[1:-1] for g in locations]
+                location = st.selectbox("Select a location:", str_locations)
+                loc_index = str_locations.index(location)
+                selected_loc = locations[loc_index]
+                lat, lng = selected_loc.lat, selected_loc.lng
+                folium.Marker(location=[lat, lng], popup=location).add_to(m)
+                m.set_center(lng, lat, 12)
+                st.session_state["zoom_level"] = 12
